e11c685d694bca048ae36085861f2e8a9f105f22
Iván Ortiz <IvanOrtiz_ITT@hotmail.com>
2017-06-30 00:35:25 -0500

    Aplicación Museografica
    Desarrollado con Unity 3D y Vuforia SDK

---
+++ Assets/Scripts/Botones.js
@@ -0,0 +1,23 @@
+﻿public function BotonInicio(){
+Application.LoadLevel("Aplicacion");
+}
+
+public function BotonInstrucciones(){
+Application.LoadLevel("Instrucciones");
+}
+
+public function BotonCreditos(){
+Application.LoadLevel("Creditos");
+}
+
+public function BotonAtras(){
+Application.LoadLevel("Menu");
+}
+
+public function BotonSalir(){
+Application.Quit();
+}
+
+public function BotonRequerimientos(){
+Application.LoadLevel("Requerimientos");
+}
---
+++ Assets/Scripts/ClickMouse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickMouse : MonoBehaviour {
+
+	public string texto = "";
+
+	public void Update(){
+
+		OnMouseDown();
+
+	}
+
+	public void OnMouseDown(){
+
+		if (Input.GetMouseButton (0)) {
+			texto = "Cargando...";
+		} else {
+			texto = "";
+		}
+
+
+	}
+}
